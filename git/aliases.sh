63fb776856cbd36912bf0011c3a79eafbccb8efd
Aakash Jain <aakashjainofficial@gmail.com>
2024-01-15 00:27:45 +0530

    Added git prompt.

---
+++ git/aliases.sh
@@ -0,0 +1,23 @@
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
+
+# --show-control-chars: help showing Korean or accented characters
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+# if using newline sep after each cmd
+alias clear="unset PROMPT_COMMAND; clear; PROMPT_COMMAND='export PROMPT_COMMAND=echo'"
+
+case "$TERM" in
+xterm*)
+	# The following programs are known to require a Win32 Console
+	# for interactive usage, therefore let's launch them through winpty
+	# when run inside `mintty`.
+	for name in node ipython php php5 psql python2.7
+	do
+		case "$(type -p "$name".exe 2>/dev/null)" in
+		''|/usr/bin/*) continue;;
+		esac
+		alias $name="winpty $name.exe"
+	done
+	;;
+esac
